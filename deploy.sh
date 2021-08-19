a49145815ac921babfceb36a1294e248caec2fe8
yulilong <yulilong222q@outlook.com>
2021-08-19 10:40:02 +0800

    部署脚本修改：github使用ssh方式推送代码

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
+# 先执行码云Pages部署
 bash mayun.sh
 
 echo '开始执行部署github命令'
@@ -12,7 +13,7 @@ echo "执行命令：cd ./.vuepress/dist\n"
 cd ./.vuepress/dist
 
 # 发布到自定义域名
-# echo "把yulilong.cn域名放到CNAME文件中"
+# echo "把yll.wiki域名放到CNAME文件中"
 echo 'yll.wiki' > CNAME
 
 # 初始化一个仓库，仅仅是做了一个初始化的操作，项目里的文件还没有被跟踪
@@ -27,17 +28,15 @@ git add -A
 echo "执行命令：commit -m 'deploy'"
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 发布到码云pages-aliyun分支上，用于阿里云拉取
 echo "代码推送到码云的pages-aliyun分支，用于阿里云拉取:"
 echo "push -f https://gitee.com/dragon-li/front-end-doc.git master:pages-aliyun"
 git push -f https://gitee.com/dragon-li/front-end-doc.git master:pages-aliyun
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-echo "执行命令：git push -f https://github.com/yulilong/front-end-doc.git master:gh-pages"
-git push -f https://github.com/yulilong/front-end-doc.git master:gh-pages
+# 发布到 GitHub Pages
+echo "执行命令：git push -f git@github.com:yulilong/front-end-doc.git master:gh-pages"
+git push -f git@github.com:yulilong/front-end-doc.git master:gh-pages
 
 # 返回到上一次的工作目录
 echo "回到刚才工作目录"
